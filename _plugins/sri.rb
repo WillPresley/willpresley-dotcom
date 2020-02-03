75418b98670ff88b5b29b99ed106eb47b0cf1396
Will Presley (amory) <william.presley.jr+git@gmail.com>
2020-02-03 15:16:53 -0500

    Add in other algorithms and also text/string versions

--- _plugins/sri.rb
+++ _plugins/sri.rb
@@ -7,7 +7,37 @@ module Jekyll
             file_path = Pathname.new(fixed_input)
             Digest::SHA512.base64digest File.read file_path
         end
+
+        def sha384(input)
+            fixed_input = input.to_s
+            file_path = Pathname.new(fixed_input)
+            Digest::SHA384.base64digest File.read file_path
+        end
+
+        def sha256(input)
+            fixed_input = input.to_s
+            file_path = Pathname.new(fixed_input)
+            Digest::SHA256.base64digest File.read file_path
+        end
+    end
+
+    module GetTextHash
+        def sha512text(input)
+            fixed_input = input.to_s
+            Digest::SHA512.base64digest fixed_input.strip
+        end
+
+        def sha384text(input)
+            fixed_input = input.to_s
+            Digest::SHA384.base64digest fixed_input.strip
+        end
+
+        def sha256text(input)
+            fixed_input = input.to_s
+            Digest::SHA256.base64digest fixed_input.strip
+        end
     end
 end
 
 Liquid::Template.register_filter(Jekyll::GetFileHash)
+Liquid::Template.register_filter(Jekyll::GetTextHash)
